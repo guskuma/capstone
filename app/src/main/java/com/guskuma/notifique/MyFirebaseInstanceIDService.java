48aa34e83b0babb3d02000c322b95f382d506680
juak <l0ndr1na0901>
2018-07-09 19:03:10 -0300

    Firebase initial config

---
+++ app/src/main/java/com/guskuma/notifique/MyFirebaseInstanceIDService.java
@@ -0,0 +1,32 @@
+package com.guskuma.notifique;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import timber.log.Timber;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // TODO send token to server
+        Timber.d("Refreshed token: %s", refreshedToken);
+//        sendRegistrationToServer(refreshedToken);
+    }
+
+}

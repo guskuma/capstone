a9f24934e16c85a094ff187a5bd8d65ab016460b
guskuma <julioguskuma@gmail.com>
2018-10-17 03:31:37 -0300

    Test implementation

--- app/src/androidTest/java/com/guskuma/notifique/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/guskuma/notifique/ExampleInstrumentedTest.java
@@ -3,11 +3,16 @@ package com.guskuma.notifique;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.guskuma.notifique.backend.messages.Messages;
+import com.guskuma.notifique.backend.messages.model.MessagePayload;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +21,30 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.guskuma.notifique", appContext.getPackageName());
+        MessagePayload msg = new MessagePayload();
+
+        Messages.Builder builder = new Messages.Builder(AndroidHttp.newCompatibleTransport(),
+                new AndroidJsonFactory(), null)
+                .setRootUrl("http://localhost:8080/_ah/api/")
+                .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                    @Override
+                    public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                        abstractGoogleClientRequest.setDisableGZipContent(true);
+                    }
+                });
+
+        Messages messagesService = builder.build();
+
+        try {
+            messagesService.sendToTopic(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
